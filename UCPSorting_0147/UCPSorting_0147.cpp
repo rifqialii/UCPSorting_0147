8ce253d0026a916d17f6733a74ae2313a99141fb
rifqialii <161274772+rifqialii@users.noreply.github.com>
2024-03-22 11:55:59 +0800

    Update UCPSorting_0147.cpp

--- UCPSorting_0147/UCPSorting_0147.cpp
+++ UCPSorting_0147/UCPSorting_0147.cpp
@@ -1,5 +1,5 @@
 // UCPSorting_0147.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//Tugas A
+//Soal A
 //1. Algoritma Insertion Sort bekerja dengan cara membandingkan elemen - elemen dalam array dan menukarnya jika diperlukan.
 //2. Algoritma ini menggunakan perbandingan untuk mencari elemen terkecil dalam daftar. Dengan cara ini, algoritma membandingkan setiap elemen dengan elemen lain dalam daftar untuk menentukan elemen yang paling kecil.
 //3. Dalam algoritma pengurutan, jumlah langkah yang dilakukan biasanya diukur dalam istilah kompleksitas waktu. Kompleksitas waktu menggambarkan jumlah operasi yang dilakukan oleh algoritma sebagai fungsi dari ukuran input.
